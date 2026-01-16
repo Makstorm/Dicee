b7542f74b4281d09e3a99b25c5ee106f20a22738
makstorm <mxm.horobets@gmail.com>
2026-01-16 15:33:45 +0100

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Maxym Horobets on 16.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
